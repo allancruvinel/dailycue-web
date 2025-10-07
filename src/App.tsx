6a1b1b66edd3692f715a153ecb2f9c657a1d95ca
allancruvinel <allan.cruvinel@gmail.com>
2025-10-06 22:38:48 -0300

    feat: create project and shadcn configuration

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from './components/ui/button'
+import './index.css'
+function App() {
+  return (
+    <div className="flex justify-center h-screen w-screen items-center">
+      <p>Inicial</p>
+      <Button className="" >Click me</Button>
+    </div>
+  )
+}
+
+export default App
