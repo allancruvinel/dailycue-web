7d844945ada139812eaa77eaebcc158cbcfa56a9
allancruvinel <allan.cruvinel@gmail.com>
2025-10-07 23:08:34 -0300

    feat: added dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { ModeToggle } from './components/mode-toggle'
 import { Button } from './components/ui/button'
 import './index.css'
+import { ThemeProvider } from "@/components/theme-provider"
 function App() {
   return (
-    <div className="flex justify-center h-screen w-screen items-center">
-      <p>Inicial</p>
-      <Button className="" >Click me</Button>
-    </div>
+    <ThemeProvider>
+      <div className="flex justify-center h-screen w-screen items-center">
+        <p>Inicial</p>
+        <ModeToggle />
+        <Button className="" >Click me</Button>
+      </div>
+    </ThemeProvider>
   )
 }
-
 export default App
