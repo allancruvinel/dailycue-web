ca690175e79044e91722f8cac0956c18771fe3f2
allancruvinel <allan.cruvinel@gmail.com>
2025-10-15 21:35:40 -0300

    feat: improved under construction page

--- src/pages/construction/construction.tsx
+++ src/pages/construction/construction.tsx
@@ -1,8 +1,11 @@
+import LogoSemBackground from '@/assets/dailycue_logo.svg'
+
 export function Construction() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-gray-800 mb-4">Página em Construção</h1>
-      <p className="text-lg text-gray-600">Estamos trabalhando para trazer novidades em breve!</p>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <img className="" src={LogoSemBackground} alt="DailyCue Logo" />
+      <h1 className="text-4xl font-bold text-primary mb-4">Página em Construção</h1>
+      <p className="text-lg text-secondary">Estamos trabalhando para trazer novidades em breve!</p>
     </div>
   )
 }
