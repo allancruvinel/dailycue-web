e0c50950ce6024b3148edcf238169f469650a57c
allancruvinel <allan.cruvinel@gmail.com>
2025-10-15 21:37:25 -0300

    fix: improved css login page

--- src/pages/auth/auth.tsx
+++ src/pages/auth/auth.tsx
@@ -3,7 +3,7 @@ import LogoSemBackground from '@/assets/dailycue_no_background.png'
 
 export const Auth = () => {
   return (
-    <div className="flex w-full gap-10 items-center justify-between">
+    <div className="flex flex-col md:flex-row w-full gap-10 items-center justify-between">
       <img className="w-1/2" src={LogoSemBackground} alt="DailyCue Logo" />
       <div className="flex-1 flex items-center justify-center">
       <LoginForm />
