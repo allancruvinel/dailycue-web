c079094c40dd978b6fe37224af85884dd1e56901
allancruvinel <allan.cruvinel@gmail.com>
2025-10-22 23:51:10 -0300

    ui: improved under construction page

--- src/pages/construction/construction.tsx
+++ src/pages/construction/construction.tsx
@@ -1,9 +1,6 @@
-import LogoSemBackground from '@/assets/dailycue_logo.svg'
-
 export function Construction() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <img className="" src={LogoSemBackground} alt="DailyCue Logo" />
       <h1 className="text-4xl font-bold text-primary mb-4">Página em Construção</h1>
       <p className="text-lg text-secondary">Estamos trabalhando para trazer novidades em breve!</p>
     </div>
