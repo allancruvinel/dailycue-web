e0790ca56b4a75da0d9a37a81849e85a74211423
allancruvinel <allan.cruvinel@gmail.com>
2025-10-25 13:27:35 -0300

    feat: added form login and register validation

--- src/pages/auth/login-form.tsx
+++ src/pages/auth/login-form.tsx
@@ -4,19 +4,78 @@ import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
 import { GoogleLogin } from "@react-oauth/google"
 import { Link } from "react-router"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from 'react-hook-form'
+import { z } from "zod"
 
 export const LoginForm = () => {
 
+   
+  const loginForm = z.object({
+    email: z.email('Email inválido'),
+    password: z.string()
+    .min(8, "A senha deve ter no mínimo 8 caracteres")
+    .max(32, "A senha deve ter no máximo 32 caracteres")
+    .regex(/[A-Z]/, "A senha deve conter pelo menos uma letra maiúscula")
+    .regex(/[a-z]/, "A senha deve conter pelo menos uma letra minúscula")
+    .regex(/[0-9]/, "A senha deve conter pelo menos um número")
+    .regex(/[^A-Za-z0-9]/, "A senha deve conter pelo menos um caractere especial"),
+  });
+
+  type LoginFormType = z.infer<typeof loginForm>;
+
+  const { register, handleSubmit,
+    formState: { errors } } = useForm<LoginFormType>(
+    {
+      resolver: zodResolver(loginForm),
+      defaultValues: {
+        email: '',
+        password: '',
+      }
+    }
+  );
+
+  const onSubmit = (data:LoginFormType) => {
+    console.log(data);
+    alert("Formulário enviado com sucesso!");
+
+    
+    //como seria caso não usasse o zodResolver
+    // const isValid = loginForm.safeParse(data);
+    // if (!isValid.success) {
+    //   isValid.error.issues.forEach((issue) => {
+    //     // seta erro manualmente
+    //     setError(issue.path[0] as keyof LoginFormType, {
+    //       type: "manual",
+    //       message: issue.message,
+    //     });
+    //   });
+    //   console.log('isValid.error.issues',isValid.error.issues);
+    //   console.log('errors',errors);
+    //   return;
+    // }
+  }
+
   return (
-    <Card className="w-sm max-w-sm h-82 p-6">
+    <Card className="w-sm max-w-sm p-6">
       <CardHeader>
         <CardTitle className="text-secondary text-bold">Login</CardTitle>
         <CardDescription className="text-primary">Não tem uma conta? <Link to="/register" className="text-indigo-400 underline">Registre-se</Link>.</CardDescription>
       </CardHeader>
       <CardContent>
-        <form>
-          <Input type="email" placeholder="Email" className="mb-4" />
-          <Input type="password" placeholder="Senha" className="mb-4" />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {errors.email && (
+            <p className="text-sm text-red-500 mt-1">
+              {errors.email.message}
+            </p>
+          )}
+          <Input type="text" {...register("email")} placeholder="Email" className="mb-4" />
+          {errors.password && (
+            <p className="text-sm text-red-500 mt-1">
+              {errors.password.message}
+            </p>
+          )}
+          <Input type="password" {...register("password")} placeholder="Senha" className="mb-4" />
           <Button type="submit" className="w-full bg-primary text-white py-2 px-4 rounded hover:bg-secondary transition-colors">
             Entrar
           </Button>
--- src/pages/auth/register-form.tsx
+++ src/pages/auth/register-form.tsx
@@ -4,21 +4,85 @@ import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
 import { GoogleLogin } from "@react-oauth/google"
 import { Link } from "react-router"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from 'react-hook-form'
+import { z } from "zod"
 
 export const RegisterForm = () => {
 
+  const registerForm = z.object({
+    name: z.string().min(2, "O nome deve ter no mínimo 2 caracteres").max(50, "O nome deve ter no máximo 50 caracteres"),  
+    email: z.email('Email inválido'),
+    password: z.string()
+    .min(8, "A senha deve ter no mínimo 8 caracteres")
+    .max(32, "A senha deve ter no máximo 32 caracteres")
+    .regex(/[A-Z]/, "A senha deve conter pelo menos uma letra maiúscula")
+    .regex(/[a-z]/, "A senha deve conter pelo menos uma letra minúscula")
+    .regex(/[0-9]/, "A senha deve conter pelo menos um número")
+    .regex(/[^A-Za-z0-9]/, "A senha deve conter pelo menos um caractere especial"),
+    confirmPassword: z.string().min(8, "A senha deve ter no mínimo 8 caracteres")
+    .max(32, "A senha deve ter no máximo 32 caracteres")
+    .regex(/[A-Z]/, "A senha deve conter pelo menos uma letra maiúscula")
+    .regex(/[a-z]/, "A senha deve conter pelo menos uma letra minúscula")
+    .regex(/[0-9]/, "A senha deve conter pelo menos um número")
+    .regex(/[^A-Za-z0-9]/, "A senha deve conter pelo menos um caractere especial"),
+    }).refine((data) => data.password === data.confirmPassword, {
+      message: "As senhas não coincidem",
+      path: ["confirmPassword"],
+    });
+
+  type RegisterFormType = z.infer<typeof registerForm>;
+
+  const { register, handleSubmit,
+    formState: { errors } } = useForm<RegisterFormType>(
+    {
+      resolver: zodResolver(registerForm),
+      defaultValues: {
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+      }
+    }
+  );
+
+  const onSubmit = (data:RegisterFormType) => {
+    console.log(data);
+    alert("Formulário enviado com sucesso!");
+  }
+
   return (
-    <Card className="w-sm max-w-sm h-107 p-6">
+    <Card className="w-sm max-w-sm p-6">
       <CardHeader>
         <CardTitle className="text-secondary text-bold">Registro</CardTitle>
         <CardDescription className="text-primary">Já tem uma conta? <Link to="/login" className="text-indigo-400 underline">Faça login</Link>.</CardDescription>
       </CardHeader>
       <CardContent>
-        <form>
-          <Input type="text" placeholder="Nome" className="mb-4" />
-          <Input type="email" placeholder="Email" className="mb-4" />
-          <Input type="password" placeholder="Senha" className="mb-4" />
-          <Input type="password" placeholder="Confirme a Senha" className="mb-4" />  
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {errors.name && (
+            <p className="text-sm text-red-500 mt-1">
+              {errors.name.message}
+            </p>
+          )}
+          <Input type="text" {...register("name")} placeholder="Nome" className="mb-4" />
+          {errors.email && (
+            <p className="text-sm text-red-500 mt-1">
+              {errors.email.message}
+            </p>
+          )}
+          <Input type="text" {...register('email')} placeholder="Email" className="mb-4" />
+          {errors.password && (
+            <p className="text-sm text-red-500 mt-1">
+              {errors.password.message}
+            </p>
+          )}
+          <Input type="password" {...register('password')} placeholder="Senha" className="mb-4" />
+          {errors.confirmPassword && (
+            <p className="text-sm text-red-500 mt-1">
+              {errors.confirmPassword.message}
+            </p>
+          )}
+          <Input type="password" {...register('confirmPassword')} placeholder="Confirme a Senha" className="mb-4" />  
           <Button type="submit" className="w-full bg-primary text-white py-2 px-4 rounded hover:bg-secondary transition-colors">
             Criar Conta
           </Button>
