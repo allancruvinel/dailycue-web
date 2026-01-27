2adb01327c5d9f2ff12521cae43ac13f844a6663
allancruvinel <allan.cruvinel@gmail.com>
2026-01-26 23:15:29 -0300

    feat: added more components and pagination

--- src/pages/chats/chats-cue-random/chats-cue-random.tsx
+++ src/pages/chats/chats-cue-random/chats-cue-random.tsx
@@ -11,6 +11,24 @@ import { DataTable } from "./cue-data-table";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import { CueColumnsFilters } from "./cue-columns-filters";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+import { useSearchParams } from "react-router";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Field, FieldLabel } from "@/components/ui/field";
 
 export type CueRandomTips = {
   id: number;
@@ -20,7 +38,7 @@ export type CueRandomTips = {
   updatedAt: string;
   selected?: boolean;
 };
-const data: CueRandomTips[] = Array.from({ length: 100 }, (_, i) => ({
+const data: CueRandomTips[] = Array.from({ length: 10 }, (_, i) => ({
   id: i + 1,
   contentText: `Dica ${i + 1}: Esta é uma dica de exemplo.`,
   isActive: i % 2 === 0,
@@ -30,8 +48,25 @@ const data: CueRandomTips[] = Array.from({ length: 100 }, (_, i) => ({
 }));
 
 export const ChatsCueRandom = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
   const [rowIndexSelection, setRowIndexSelection] = useState({});
   const lineSelected = data.filter((item) => item.selected).length;
+
+  const handlePaginate = (page: number) => {
+    setSearchParams((prev) => {
+      prev.set("page", page.toString());
+      return prev;
+    });
+  };
+
+  const handlePageSizeChange = (pageSize: number) => {
+    setSearchParams((prev) => {
+      prev.set("pageSize", pageSize.toString());
+      return prev;
+    });
+  };
+
   return (
     <div className="flex flex-col items-center justify-center w-full p-4">
       <div className="w-full flex flex-row items-center justify-between">
@@ -82,6 +117,50 @@ export const ChatsCueRandom = () => {
           rowSelection={rowIndexSelection}
           onRowSelectionChange={setRowIndexSelection}
         />
+        <Pagination className="mt-4">
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious onClick={() => handlePaginate(1 - 1)} />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink onClick={() => handlePaginate(1)}>
+                1
+              </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink onClick={() => handlePaginate(2)} isActive>
+                2
+              </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink onClick={() => handlePaginate(3)}>
+                3
+              </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationNext onClick={() => handlePaginate(1 + 1)} />
+            </PaginationItem>
+          </PaginationContent>
+          <Field orientation="horizontal" className="w-fit">
+            <Select
+              defaultValue="25"
+              onValueChange={(e) => handlePageSizeChange(Number(e))}
+            >
+              <SelectTrigger className="w-20" id="select-rows-per-page">
+                <SelectValue />
+              </SelectTrigger>
+              <SelectContent align="start">
+                <SelectGroup>
+                  <SelectItem value="10">10</SelectItem>
+                  <SelectItem value="25">25</SelectItem>
+                  <SelectItem value="50">50</SelectItem>
+                  <SelectItem value="100">100</SelectItem>
+                  <SelectItem value="400">400</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </Field>
+        </Pagination>
       </div>
     </div>
   );
