479828f3bd0e9f0f42ac5be1b9087dc4adfe7f21
allancruvinel <allan.cruvinel@gmail.com>
2026-02-25 22:56:14 -0300

    feat: added schedule cue part 1 - single-send

--- src/pages/chats/chats-details-layout.tsx
+++ src/pages/chats/chats-details-layout.tsx
@@ -15,6 +15,9 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Separator } from "@/components/ui/separator";
 import { Textarea } from "@/components/ui/textarea";
 import { X } from "lucide-react";
 import { useState } from "react";
@@ -28,6 +31,8 @@ export const ChatsDetailsLayout = () => {
 
   const [newRandomCues, setNewRandomCues] = useState<string[]>([]);
   const [inputRandomCuesText, setInputRandomCuesText] = useState("");
+
+  const [inputScheduleCueText, setInputScheduleCueText] = useState("");
 
   const handleAddRandomCues = (cuesText: string) => {
     setInputRandomCuesText(cuesText);
@@ -62,8 +67,44 @@ export const ChatsDetailsLayout = () => {
                     Crie um novo cue para interagir com seus contatos.
                   </DialogDescription>
                 </DialogHeader>
-                <Input placeholder="Nome do cue"></Input>
-                <Input placeholder="Descrição do cue"></Input>
+                <Textarea
+                  value={inputScheduleCueText}
+                  onChange={(e) => setInputScheduleCueText(e.target.value)}
+                  className="min-h-24"
+                  placeholder="insira a frase do cue programado aqui"
+                ></Textarea>
+                <Separator className="my-4" />
+                <RadioGroup defaultValue="single-send" className="w-fit">
+                  <div className="flex items-center gap-3">
+                    <RadioGroupItem value="single-send" id="r1" />
+                    <Label htmlFor="r1">Enviar uma única vez</Label>
+                  </div>
+                  <div className="flex items-center gap-3">
+                    <RadioGroupItem value="daily-send" id="r2" />
+                    <Label htmlFor="r2">Repetir diariamente</Label>
+                  </div>
+                  <div className="flex items-center gap-3">
+                    <RadioGroupItem value="weekly-send" id="r3" />
+                    <Label htmlFor="r3">Repetir semanalmente</Label>
+                  </div>
+                  <div className="flex items-center gap-3">
+                    <RadioGroupItem value="monthly-send" id="r4" />
+                    <Label htmlFor="r4">Repetir mensalmente</Label>
+                  </div>
+                  <div className="flex items-center gap-3">
+                    <RadioGroupItem value="custom-send" id="r5" />
+                    <Label htmlFor="r5">Personalizado</Label>
+                  </div>
+                </RadioGroup>
+                <Separator className="my-4" />
+
+                <Label htmlFor="schedule">
+                  Escolha a data e hora para o envio do cue programado
+                </Label>
+
+                <Input id="schedule" type="datetime-local" className="w-full" />
+                <Separator className="my-4" />
+
                 <DialogFooter>
                   <Button type="submit">Salvar</Button>
                 </DialogFooter>
